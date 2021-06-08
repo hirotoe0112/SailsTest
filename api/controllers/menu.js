56d8935aa19d34be7bf1b0cfcfcafee366325a21
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2021-06-08 23:16:05 +0900

    日付表示

--- api/controllers/menu.js
+++ api/controllers/menu.js
@@ -17,13 +17,21 @@ module.exports = {
     success: {
       viewTemplatePath: 'home/menu'
     }
+
   },
 
 
   fn: async function (inputs) {
 
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth() + 1;
+    const currentDay = currentDate.getDate();
+
     // All done.
-    return;
+    return{
+      day: currentYear + '/' + currentMonth + '/' + currentDay
+    };
 
   }
 
